35cd144f307988a83c83e0a521a5679dccee3147
VolodymyrS <volodymyr.sh.05@gmail.com>
2024-04-05 11:08:13 +0300

    added get_same/get_different_content functions

--- funcs/read_content.py
+++ funcs/read_content.py
@@ -1,18 +1,50 @@
 def read_content_from_file(filepath: str):
     """
-    Зчитує вміст файлу і повертає його у вигляді списку рядків.
+    Reads the contents of a file and returns it as a list of lines.
 
     Args:
-        filepath (str): Шлях до файлу, який потрібно зчитати.
+        filepath (str): The path to the file to be read.
 
     Returns:
-        list: Список, що містить рядки файлу.
+        list: A list containing the lines of the file.
 
     Raises:
-        FileNotFoundError: Якщо файл, вказаний за допомогою 'filepath', не існує.
-        PermissionError: Якщо у користувача немає прав на читання файлу.
-        UnicodeDecodeError: Якщо виникає проблема з декодуванням вмісту файлу.
-        OSError: Для будь-яких інших пов'язаних з операційною системою помилок.
+        FileNotFoundError: If the file specified by 'filepath' does not exist.
+        PermissionError: If the user does not have permission to read the file.
+        UnicodeDecodeError: If there's an issue decoding the file's contents.
+        OSError: For any other operating system related errors.
     """
     with open(filepath, "r") as f_o:
         return f_o.readlines()
+
+
+def get_same_content_from_files(filepath1, filepath2):
+    """
+    Reads the contents of two files and returns a set of lines that are present in both files.
+
+    Args:
+        filepath1 (str): The path to the first file.
+        filepath2 (str): The path to the second file.
+
+    Returns:
+        set: A set of lines present in both the first and the second file.
+
+    Raises:
+        FileNotFoundError: If one of the files is not found.
+        PermissionError: If the user does not have permission to access the files.
+        UnicodeDecodeError: If there is an issue decoding the contents of the files.
+        OSError: For any other operating system related errors while working with files.
+    """
+    file1 = read_content_from_file(filepath1)
+    file2 = read_content_from_file(filepath2)
+    # Search the same lines
+    same_lines = set(file1) & set(file2)
+    return same_lines
+
+
+def get_different_content_from_files(filepath1, filepath2):
+    file1 = read_content_from_file(filepath1)
+    file2 = read_content_from_file(filepath2)
+    # Finding lines that occur only in one of the files
+    diff_lines = set(file1) ^ set(file2)
+    return diff_lines
